3d831234d5311f31c7148e25c6da77795c5c4fe1
WIKKI <uzkingshark@gmail.com>
2026-03-02 17:02:14 +0500

    chore(assets): bump tutor js/css cache version to v34

--- erpnext_ai_tutor/hooks.py
+++ erpnext_ai_tutor/hooks.py
@@ -25,12 +25,12 @@ app_license = "Apache-2.0"
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/erpnext_ai_tutor/css/ai_tutor_widget.css?v=33"
+app_include_css = "/assets/erpnext_ai_tutor/css/ai_tutor_widget.css?v=34"
 app_include_js = [
-	"/assets/erpnext_ai_tutor/js/ai_tutor/utils.js?v=33",
-	"/assets/erpnext_ai_tutor/js/ai_tutor/guide.js?v=33",
-	"/assets/erpnext_ai_tutor/js/ai_tutor/widget.js?v=33",
-	"/assets/erpnext_ai_tutor/js/ai_tutor/boot.js?v=33",
+	"/assets/erpnext_ai_tutor/js/ai_tutor/utils.js?v=34",
+	"/assets/erpnext_ai_tutor/js/ai_tutor/guide.js?v=34",
+	"/assets/erpnext_ai_tutor/js/ai_tutor/widget.js?v=34",
+	"/assets/erpnext_ai_tutor/js/ai_tutor/boot.js?v=34",
 ]
 app_include_icons = ["erpnext_ai_tutor/icons.svg"]
 
